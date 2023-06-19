a2d175d9b510a8870b2d1c874353f5eb6e8cd712
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-06-19 12:01:38 +0200

    changement du nom de la variable patientId

--- src/main/java/com/OpenClassroom/MedicalRecords/Repository/IMedicalNoteRepository.java
+++ src/main/java/com/OpenClassroom/MedicalRecords/Repository/IMedicalNoteRepository.java
@@ -2,13 +2,14 @@ package com.OpenClassroom.MedicalRecords.Repository;
 
 import com.OpenClassroom.MedicalRecords.Model.MedicalNoteEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface IMedicalNoteRepository extends MongoRepository<MedicalNoteEntity, Integer> {
-    List<MedicalNoteEntity> findByPatientId(Integer patient_id);
+public interface IMedicalNoteRepository extends CrudRepository<MedicalNoteEntity, Integer> {
+    List<MedicalNoteEntity> findByPatientId(Integer patientId);
 
     List<MedicalNoteEntity> findByPatientLastName(String patientLastName);
 }
